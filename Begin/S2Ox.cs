0b0686bb3997c610d0c87dbe9f417f75ab352471
wroblewskiand <wroblewski.and@gmail.com>
2023-07-19 23:56:09 +0200

    S2Ox

--- Begin/S2Ox.cs
+++ Begin/S2Ox.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿// NAME
+
+// /*
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,12 +12,11 @@ namespace Begin
 {
     internal class S2Ox
     {
-        /*
         static void Main(string[] args)
         {
 
         }   
-        */
-
     }
 }
+
+// */

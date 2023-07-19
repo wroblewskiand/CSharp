cc6fd181557bc84f64cae94136ae3a0841e47fe2
wroblewskiand <wroblewski.and@gmail.com>
2023-07-20 00:02:38 +0200

    NUM

---
+++ Begin/S2O7.cs
@@ -0,0 +1,37 @@
+﻿// SWITCH STATEMENT
+
+ /*
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Begin
+{
+    internal class S2O6
+    {
+        static void Main(string[] args)
+        {
+            switch (DateTime.Now.DayOfWeek)
+            {
+                case DayOfWeek.Monday:
+                    Console.WriteLine("It's Monday ;(");
+                    break;
+                case DayOfWeek.Friday:
+                    Console.WriteLine("The last day of the work week.");
+                    break;
+                case DayOfWeek.Sunday:
+                case DayOfWeek.Saturday:
+                    Console.WriteLine("The weekend!");
+                    break;
+                default:
+                    Console.WriteLine("The middle of the work week.");
+                    break;
+            }
+        }   
+    }
+}
+
+ */

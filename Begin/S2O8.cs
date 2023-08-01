4784b2abba11106c4e89c07ce8b2e9600b9024ee
wroblewskiand <wroblewski.and@gmail.com>
2023-08-01 14:14:15 +0200

    S2O8

--- Begin/S2O8.cs
+++ Begin/S2O8.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 
 namespace Begin
@@ -14,23 +15,46 @@ namespace Begin
     {
         static void Main(string[] args)
         {
+            //    Console.WriteLine("Enter \"byte value\":");
+            //    string stringValue = Console.ReadLine();
+            //    int intValue = int.Parse(stringValue);
+            //    byte byteValue = (byte) intValue;
+            //    Console.WriteLine(byteValue);
+            //    if (byte.TryParse(stringValue, out byteValue))
+            //    {
+            //        //int intValue = byteValue;
+            //        byte byteValue2 = (byte)intValue;
+            //        // byte byteValue2 = intValue;      // nie można niejawnie (implicity) rzutować int na byte
+            //        Console.WriteLine(byteValue);
+            //        Console.WriteLine(intValue);
+            //        Console.WriteLine(byteValue2);
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("out of range");
+            //    }
+            // float floatValue = float.Parse(userInput);       // parsowanie/rzutowanie na inny typ -> float 
+
+            byte byteValue = 100;
+            int intValue = byteValue;
+            byte byteValue2 = (byte)intValue;
+            double doubleValue = 3.5;
+            int intValue2 = (int)doubleValue;
+
+            string stringValue2 = intValue.ToString();
+
+            Console.WriteLine(stringValue2);
+             
+            Console.WriteLine("Write year of yours birth:");
             string userInput = Console.ReadLine();
-
-
-            // metoda 1
-            // int yearOfBirth = int.Parse(userInput);
-            // float floatValue = float.Parse(userInput);
-            // short shortValue = short.Parse(userInput);
-            // int age = DateTime.Now.Year - yearOfBirth;
-            // Consol.WriteLine("You are " + age);
-
-
-            // metoda 2
             int yearOfBirth;
-            if (int.TryParse(userInput, out yearOfBirth))
+
+            if (int.TryParse(userInput, out yearOfBirth)) 
             {
                 int age = DateTime.Now.Year - yearOfBirth;
-                Console.WriteLine("You are " + age);
+
+                Console.WriteLine("You are " + age); 
+            
             }
             else
             {

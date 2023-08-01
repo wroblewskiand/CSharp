8b21d73976f31dc889d07a68f9e593ec8ddb8f92
wroblewskiand <wroblewski.and@gmail.com>
2023-08-01 21:51:52 +0200

    S2O8zadanie begin

--- Begin/S2O8zadanie.cs
+++ Begin/S2O8zadanie.cs
@@ -1,6 +1,6 @@
-﻿// Konwersja typów i rzutowanie
+﻿// Utwórz aplikację konsolową która pobierze od użytkownika datę jego urodzin i na tej podstawie obliczy ile mięło dni od tej daty do obecnego dnia.
 
-/*
+// /*
 
 using System;
 using System.Collections.Generic;
@@ -15,53 +15,10 @@ namespace Begin
     {
         static void Main(string[] args)
         {
-            //    Console.WriteLine("Enter \"byte value\":");
-            //    string stringValue = Console.ReadLine();
-            //    int intValue = int.Parse(stringValue);
-            //    byte byteValue = (byte) intValue;
-            //    Console.WriteLine(byteValue);
-            //    if (byte.TryParse(stringValue, out byteValue))
-            //    {
-            //        //int intValue = byteValue;
-            //        byte byteValue2 = (byte)intValue;
-            //        // byte byteValue2 = intValue;      // nie można niejawnie (implicity) rzutować int na byte
-            //        Console.WriteLine(byteValue);
-            //        Console.WriteLine(intValue);
-            //        Console.WriteLine(byteValue2);
-            //    }
-            //    else
-            //    {
-            //        Console.WriteLine("out of range");
-            //    }
-            // float floatValue = float.Parse(userInput);       // parsowanie/rzutowanie na inny typ -> float 
+            Console.WriteLine("Write yours birth date");
 
-            byte byteValue = 100;
-            int intValue = byteValue;
-            byte byteValue2 = (byte)intValue;
-            double doubleValue = 3.5;
-            int intValue2 = (int)doubleValue;
-
-            string stringValue2 = intValue.ToString();
-
-            Console.WriteLine(stringValue2);
-             
-            Console.WriteLine("Write year of yours birth:");
-            string userInput = Console.ReadLine();
-            int yearOfBirth;
-
-            if (int.TryParse(userInput, out yearOfBirth)) 
-            {
-                int age = DateTime.Now.Year - yearOfBirth;
-
-                Console.WriteLine("You are " + age); 
-            
-            }
-            else
-            {
-                Console.WriteLine("Incorrect value");
-            }
         }   
     }
 }
 
-*/
+// */
